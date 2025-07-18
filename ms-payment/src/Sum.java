cfcbc5e20df1b0d748bc8ab8451b925bbdbaf780
matanatt <teyyubovametanet@gmail.com>
2025-07-18 15:45:10 +0400

    feature:longest common prefix

--- ms-payment/src/Sum.java
+++ ms-payment/src/Sum.java
@@ -34,36 +34,52 @@ public class Sum {
 //    }
 
 
+//    public static void main(String[] args) {
+//        String s= "XIV";
+//        int result =0;
+//        int prev=0;
+//        for(int i=s.length()-1;i>=0;i--){
+//            char c=s.charAt(i);
+//            int current =0;
+//
+//            if (c == 'I') current = 1;
+//            else if (c == 'V') current = 5;
+//            else if (c == 'X') current = 10;
+//            else if (c == 'L') current = 50;
+//            else if (c == 'C') current = 100;
+//            else if (c== 'D') current = 500;
+//            else if (c == 'M') current = 1000;
+//
+//            if(current <prev){
+//                result-= current;
+//            }else{
+//                result+= current;
+//            }
+//
+//
+//            prev= current;
+//
+//
+//        }
+//
+//        System.out.println(result);
+//    }
+
+
     public static void main(String[] args) {
-        String s= "XIV";
-        int result =0;
-        int prev=0;
-        for(int i=s.length()-1;i>=0;i--){
-            char c=s.charAt(i);
-            int current =0;
 
-            if (c == 'I') current = 1;
-            else if (c == 'V') current = 5;
-            else if (c == 'X') current = 10;
-            else if (c == 'L') current = 50;
-            else if (c == 'C') current = 100;
-            else if (c== 'D') current = 500;
-            else if (c == 'M') current = 1000;
+            String[] strs = new String[] {"flower", "flow", "flight"};
+            for(int i=0;i< strs.length ;i++){
 
-            if(current <prev){
-                result-= current;
-            }else{
-                result+= current;
+              String prefix = strs[0];
+              while(strs[i].indexOf(prefix) != 0){
+                  prefix =prefix.substring(0,prefix.length()-1);
+              }
+                System.out.println(prefix);
             }
 
 
-            prev= current;
 
-
-        }
-
-        System.out.println(result);
     }
-
 
 }
